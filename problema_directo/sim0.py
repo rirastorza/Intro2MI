c20f30552914bdb141262cb7ce98d1e16998b977
rirastorza <50217531+rirastorza@users.noreply.github.com>
2023-04-19 17:04:24 -0300

    medidas de calibracion

--- problema_directo/sim0.py
+++ problema_directo/sim0.py
@@ -1,61 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Uso del módulo fordward
-# 
-# En el módulo *forward* se implementan algunas formas de resolución del problema directo de imágenes por microondas de un arreglo circular de antenas. Para el caso analítico solo se considera el caso en que el dispersor es un cilindro centrado. En los casos del método de diferencias finitas (FDTD), elementos finitos (FEM), y el método de los momentos (MoM) se pueden generar geometrías más complejas.
-# 
-# - FDTD está implementado en el software [meep](https://meep.readthedocs.io/en/latest/).
-# 
-# - FEM está implementado en el software [FEniCS](https://fenicsproject.org/)***(IMPLEMENTAR).
-# 
-# - MoM está implementado completo basado en [1]***(IMPLEMENTAR).
-# 
-# Libros y publicaciones:
-# 
-# [1] Xudong Chen, Computational Methods for Electromagnetic Inverse Scattering
-# 
-# [2] Matteo Pastorino, Microwave Imaging
-# 
-# 
-# Módulo Python: forward
-# 
-# Autores: Julián Galván & Ramiro Irastorza 
-# 
-# Email: rirastorza@iflysib.unlp.edu.ar
-# 
-
-# ## Módulo forward **COMPLETAR
-# 
-# Breve descripción del módulo. Si se ejecuta solo con la instrucción:
-# 
-# > python3 forward.py
-# 
-# A continuación se comentan varios ejemplos de simulación.
-# 
-# ## Ejemplo de uso para calibración
-# 
-# ### Conversión de parámetros $S_{12}$ a campo eléctrico $E_{z}$ y calibración
-# 
-# El coeficiente de transmisión entre antena transmisora (tx) y receptora (tr) se denomina parámetro $S_{12}$. Como los algoritmos de reconstrucción de imágenes siempre utilizan el campo eléctrico entonces es necesario traducir la información medida ($S_{12}$) a la teórica ($E_{z}$, en nuestro caso porque utilizamos algorimos 2D en configuración TM). Según Ostadrahimi M. y otros (2011) [enlace](https://www.researchgate.net/profile/Amer_Zakaria/publication/224257030_Analysis_of_Incident_Field_Modeling_and_IncidentScattered_Field_Calibration_Techniques_in_Microwave_Tomography/links/0912f50a51f7a51be0000000.pdf), esto se puede hacer calculando los coeficientes de calibración ($c_{F}$) en cada uno de los **receptores** para cada uno de los **transmisores**. Con el cálculo de estos coeficientes también se pueden compesar algunos otros errores debido a diferentes longitudes de camino. Se puede hacer de dos maneras:
-# 
-# 1. Con el campo incidente. La expresión en este caso es la siguiente:
-# 
-# $$c_{F_{ls}} = \dfrac{E_{esperado}^{inc}}{S_{medido}^{inc}}$$
-# 
-# donde $E_{esperado}^{inc}$ es el campo simulado o calculado analíticamene en cada receptor y $S_{medido}^{inc}$ el coeficiente de dispersión medido. El subíndice _ls_ es de line source.
-# 
-# 2. Con el campo disperso por un medio canónico, por ejemplo, cilindro centrado en (0,0) con dieléctrico conocido. Se reemplaza _inc_ por el _sct_, es decir, el campo eléctrico calculado y el coeficiente de dispersión con en el centro (_problema canónico_).
-# 
-# En este jupyter notebook vamos a calcular las dos formas. Vamos utilizar los $S_{medido}$ con nuestro setup experimental y además también vamos a simular los parámetros $S$ en 3D con FEM, luego compararemos con los valores obtenidos con simulación 2D de los campos eléctricos simulados con FDTD.
-# 
-# ### Simulación
-# 
-# En lo que sigue, simularemos tanto el campo incidente (sólo con el medio de acoplamiento) y el campo con el cilindro canónico (cilindro de teflón de diámetro 25 mm centrado en (0,0)). A continuación, mostramos cómo hacer la simulación con la librería _forward_.
-
-# In[1]:
-
-
 from forward import *
 import time as tm
 import os
@@ -64,20 +9,51 @@ from numpy import genfromtxt
 import matplotlib.pyplot as plt
 
 
-# Iniciamos el contador para estimar cuanto tarda la simulación del modelo.
+def agrupa(datos,nfrec,nant):
+    """
+    Agrupa los datos medidos de la siguiente forma (para nant = 8):
+    #Med_P1P2  Med_P1P3  Med_P1P4  Med_P1P5  Med_P1P6  Med_P1P7  Med_P1P8  
+    #Med_P2P3  Med_P2P4  Med_P2P5  Med_P2P6  Med_P2P7  Med_P2P8  Med_P1P2
+    #Med_P3P4  Med_P3P5  Med_P3P6  Med_P3P7  Med_P3P8  Med_P1P3  Med_P2P3  
+    #Med_P4P5  Med_P4P6  Med_P4P7  Med_P4P8  Med_P1P4  Med_P2P4  Med_P3P4
+    #Med_P5P6  Med_P5P7  Med_P5P8  Med_P1P5  Med_P2P5  Med_P3P5  Med_P4P5
+    #Med_P6P7  Med_P6P8  Med_P1P6  Med_P2P6  Med_P3P6  Med_P4P6  Med_P5P6
+    #Med_P7P8  Med_P1P7  Med_P2P7  Med_P3P7  Med_P4P7  Med_P5P7  Med_P6P7
+    #Med_P1P8  Med_P2P8  Med_P3P8  Med_P4P8  Med_P5P8  Med_P6P8  Med_P7P8
+    """
+    Sinci = np.zeros((len(datos[0,1:nant+1]),len(datos[0,1:nant+1])))
+    nant_anterior = 1
+    for i in range(nant-1):
+        #print(i,nant-i-1,nant_anterior,nant_anterior+nant-i)
+        Sinci[i,:nant-i-1] = datos[nfrec,nant_anterior:nant_anterior+nant-i-1] 
+        nant_anterior = nant_anterior+nant-i-1
+    for i in range(nant-1):
+        rows, cols = np.indices((len(datos[0,1:nant+1]),len(datos[0,1:nant+1])))
+        row_vals = np.diag(np.fliplr(rows), k=-i)
+        col_vals = np.diag(np.fliplr(cols), k=-i)
+        Sinci[row_vals, col_vals]= Sinci[i,0:nant-i]
 
-# In[2]:
+    return Sinci[:,:-1]
 
+
+
+incidente = 'MED_NaCL_8pos_sin_teflon_Npos_8_04-Apr-2023_15h_11m_30s_pos_completo_col.txt'
+canonico = 'MED_NaCL_8pos_teflon_Npos_8_04-Apr-2023_14h_44m_26s_pos_completo_col.txt'
+
+datos = np.loadtxt(incidente,skiprows=1)
+f_medida = 1e6*datos[:,0]
+datos_canonico = np.loadtxt(canonico,skiprows=1,delimiter=',')
+
+nfrec = 64
+nant = 8
+Sinc_FINAL = agrupa(datos,nfrec,nant)
+S_FINAL = agrupa(datos_canonico,nfrec,nant)
+
+f = f_medida[nfrec]
 
 start_time = tm.strftime('%H:%M:%S')
 
-
-# Se define la frecuencia, el tamaño de la caja de simulación 25 cm x 25 cm, donde está el arreglo de n antenas (en el ejemplo 4 antenas). Se crean los objetos _TRANSMISOR_parameters_ y _ACOPLANTE_parameters_ y se les asignan las propiedades. Estos objetos contienen información del arreglo de antenas y del fluido de acoplamiento, respectivamente. El acoplante, en este ejemplo, es agua con sal cuyo modelo dieléctrico es un Debye (una frecuencia de relajación) con conductividad iónica.
-
-# In[3]:
-
-
-f = 0.6e9
+#f = 0.6e9
 print('frecuencia de medición (GHz): ',f/1e9)
 sx = 0.3
 sy = 0.3
@@ -87,7 +63,7 @@ TRANSMISOR_parameters = TRANSMISOR_parameters()
 TRANSMISOR_parameters.f = f
 TRANSMISOR_parameters.amp = 7.5e4
 TRANSMISOR_parameters.rhoS = 0.075
-TRANSMISOR_parameters.S = 4.
+TRANSMISOR_parameters.S = 8.
 
 #Debye para graficar
 frel = 20e9
@@ -115,14 +91,8 @@ ACOPLANTE_parameters.epsr = epsc.real
 ACOPLANTE_parameters.sigma = -epsc.imag*(eps0*2*pi*TRANSMISOR_parameters.f)
 #plt.show()
 
-
-# Luego se define el cilindro dispersor pasando los parámetros al objeto _SCATTERER_parameters_. El cilindro está centrado en (0,0) y es de teflon (permitividad relativa 2.1 y conductividad nula). Aquí también lo dibujamos, junto con el arreglo de antenas.
-
-# In[4]:
-
-
 #Coordenadas antenas
-angulo = N.linspace(0.0, 2.0*pi, 5)
+angulo = N.linspace(0.0, 2.0*pi, 9)
 xantenas = (TRANSMISOR_parameters.rhoS)*N.cos(angulo)
 yantenas = (TRANSMISOR_parameters.rhoS)*N.sin(angulo)
 
@@ -160,147 +130,170 @@ axes.plot(xantenas,yantenas,'ok')
 #plt.show()
 
 
-# Ahora simulamos, utilizando el transmisor número 2 (_tx = 2_) luego seleccionamos el campo en el receptor 0 (_t_r=0_). De esta manera obtenemos $E_{esperado}^{inc}$ y $E_{esperado}^{sct}$.
-
-# In[5]:
-
-
 resolucion = 5
 n = resolucion*sx/a
-tx = 2
+tx = 0
 Ezfdtd,eps_data = RunMeep(cilindro1,ACOPLANTE_parameters,TRANSMISOR_parameters, tx, box,RES = 5,calibration = False)
 Ezfdtdinc,eps_data_no = RunMeep(cilindro1,ACOPLANTE_parameters,TRANSMISOR_parameters, tx, box,RES = 5,calibration = True)
 
-tr = 0
-xRint = int(resolucion*((TRANSMISOR_parameters.rhoS)*N.cos(tr*2*pi/4.))/a)+int(n/2) #Coordenada x antena emisora
-yRint = int(resolucion*((TRANSMISOR_parameters.rhoS)*N.sin(tr*2*pi/4.))/a)+int(n/2)
+EzTr = np.zeros(nant)
+EzTrinc = np.zeros(nant)
+print(len(xantenas))
+for tr in range(nant):
+    print('Coordenadas de antenas:',tr+1,':(',xantenas[tr],-yantenas[tr],')')
+    xRint = int(resolucion*(xantenas[tr])/a)+int(n/2) #Coordenada x antena emisora
+    yRint = int(resolucion*(-yantenas[tr])/a)+int(n/2)
+    EzTr[tr] = abs(Ezfdtd)[xRint,yRint]
+    EzTrinc[tr] = abs(Ezfdtdinc)[xRint,yRint]
 
-EzTr = abs(Ezfdtd)[xRint,yRint]
-EzTrinc = abs(Ezfdtdinc)[xRint,yRint]
+#plt.figure()
+#extent2=[-0.3/2,0.3/2,-0.3/2,0.3/2]
+#plt.imshow(abs(Ezfdtd).transpose(),extent = extent2)#cmap = 'binary')
+#plt.plot(xantenas,yantenas,'ow')
+#plt.colorbar()
 
-plt.figure()
-extent2=[-0.3/2,0.3/2,-0.3/2,0.3/2]
-plt.imshow(abs(Ezfdtd).transpose(),extent = extent2)#cmap = 'binary')
-plt.plot(xantenas,yantenas,'ow')
-plt.colorbar()
-
-#plt.show()
-
-
-# Ahora calculamos los coeficientes de calibración $c_{F_{ls}}^{inc}$ y $c_{F_{ls}}^{sct}$ con los valores medidos en el setup experimental (archivo: _Med_pos_1_pos_3_prueba1.txt_, son las antenas opuestas).
-
-# In[6]:
+##plt.show()
 
 
-canonico = 'MED_NaCL_8pos_teflon_Npos_4_04-Apr-2023_15h_2m_53s_pos_completo_col.txt'#Cilindro de teflon
-datos = np.loadtxt(canonico,skiprows=1)
-f_medida = datos[:,0]
-S_medida = datos[:,1]
+fig10 = plt.figure(10)
+ax1 = fig10.add_subplot(121)
+ax1.plot(Sinc_FINAL[0,:],'ob-',label='Incidente, Tx: 1, f: '+str(f_medida[nfrec]/1e6)+' MHz')
+#ax1.plot(Sinc_FINAL[1,:],'xr-',label='Incidente, Tx: 2')#, f: '+str(f_medida[nfrec]/1e6)+' MHz')
+ax1.plot(S_FINAL[0,:],'ob--',label='Teflon, Tx: 1, f: '+str(f_medida[nfrec]/1e6)+' MHz')
+#ax1.plot(S_FINAL[1,:],'xr--',label='Teflon, Tx: 2')#, f: '+str(f_medida[nfrec]/1e6)+' MHz')
+ax1.set_ylim([-65, -35])
+ax1.legend(loc="upper right")
+ax1.set_xticks([0, 1, 2, 3, 4, 5, 6])
+ax1.set_xticklabels(['12', '13', '14', '15', '16', '17', '18']) 
 
+ax2 = fig10.add_subplot(122)
 
-fig2 = plt.figure(5)
-ax1 = fig2.add_subplot(121)
-ax1.semilogx(f_medida,S_medida,'-k')
-#ax1.set_xlim([0.6e9, 1.6e9])
-#ax1.set_ylabel('Parte real permitividad')
-#ax1.set_xlabel('Frecuencia (Hz)')
-#ax2 = fig2.add_subplot(122)
-#ax2.semilogx(frec,-epsc.imag,'-b')
-#ax2.set_xlim([0.6e9, 1.6e9])
-#ax2.set_ylabel('Parte imaginaria permitividad')
-#ax2.set_xlabel('Frecuencia (Hz)')
+ax2.plot(20.0*np.log10(EzTrinc[1:]/EzTrinc[0]),'o-',label='Incidente, f: '+str(f_medida[nfrec]/1e6)+' MHz')
+ax2.plot(20.0*np.log10(EzTr[1:]/EzTrinc[0]),'x--',label='Teflon')#, f: '+str(f_medida[nfrec]/1e6)+' MHz')
+#ax2.set_ylim([-65, -35])
+ax2.legend(loc="upper right")
+ax2.set_xticks([0, 1, 2, 3, 4, 5, 6])
+ax2.set_xticklabels(['12', '13', '14', '15', '16', '17', '18']) 
 
 plt.show()
 
-#csct_F = abs(EzTr)/abs(10**(S_medida/20))
-
-#print('c_F :', csct_F)
 
 
-#print('start time: ', start_time)
-#print('end time:   ', tm.strftime('%H:%M:%S'))
+
+## Ahora calculamos los coeficientes de calibración $c_{F_{ls}}^{inc}$ y $c_{F_{ls}}^{sct}$ con los valores medidos en el setup experimental (archivo: _Med_pos_1_pos_3_prueba1.txt_, son las antenas opuestas).
+
+## In[6]:
 
 
-## Ahora simulamos el cilindro de Nylon descentrado con lo cual tendremos un valor teórico y, luego, vemos cuál es el valor medido y calibrado con el coeficiente $c_{F_{ls}}^{sct}$.
+#canonico = 'MED_NaCL_8pos_teflon_Npos_4_04-Apr-2023_15h_2m_53s_pos_completo_col.txt'#Cilindro de teflon
+#datos = np.loadtxt(canonico,skiprows=1)
+#f_medida = datos[:,0]
+#S_medida = datos[:,1]
 
-## In[7]:
 
+#fig2 = plt.figure(5)
+#ax1 = fig2.add_subplot(121)
+#ax1.semilogx(f_medida,S_medida,'-k')
+##ax1.set_xlim([0.6e9, 1.6e9])
+##ax1.set_ylabel('Parte real permitividad')
+##ax1.set_xlabel('Frecuencia (Hz)')
+##ax2 = fig2.add_subplot(122)
+##ax2.semilogx(frec,-epsc.imag,'-b')
+##ax2.set_xlim([0.6e9, 1.6e9])
+##ax2.set_ylabel('Parte imaginaria permitividad')
+##ax2.set_xlabel('Frecuencia (Hz)')
 
-#r = 35.5e-3/2.0
-#Xc = -0.025
-#Yc = -0.025
-
-#print('Xc:', Xc,'Yc:', Yc,'r:',r)
-
-##Dibujo la geometría generada
-#cilindro = plt.Circle((Xc,Yc),r,fill = False)
-
-##Cargo los parámetros del cilindro
-#cilindro1 = SCATTERER_parameters()
-#cilindro1.epsr = 4.1 #permitividad relativa. Entre [10.0, 80.0]
-#cilindro1.sigma = 0.0
-#cilindro1.f = TRANSMISOR_parameters.f #frecuencia 1.0 GHz (por defecto).
-#cilindro1.radio = r
-#cilindro1.xc = Xc
-#cilindro1.yc = Yc
-
-#print('Permitividad medio:',ACOPLANTE_parameters.epsr)
-#print('Conductividad medio:',ACOPLANTE_parameters.sigma)
-
-#print('Permitividad del cilindro:',cilindro1.epsr)
-#print('Conductividad del cilindro:',cilindro1.sigma)
-
-#figure, axes = plt.subplots()
-#plt.xlim(-0.25/2, 0.25/2)
-#plt.ylim(-0.25/2 , 0.25/2)
-#axes.set_aspect(1)
-#axes.add_artist(cilindro)
-#axes.plot(xantenas,yantenas,'ok')
 #plt.show()
 
+##csct_F = abs(EzTr)/abs(10**(S_medida/20))
 
-## In[8]:
-
-
-#Ezfdtd_sct,eps_data = RunMeep(cilindro1,ACOPLANTE_parameters,TRANSMISOR_parameters, tx, box,RES = 5,calibration = False)
-#EzTr_sct = abs(Ezfdtd_sct)[xRint,yRint]
+##print('c_F :', csct_F)
 
 
-## Ahora comparamos con la medición:
-
-## In[9]:
-
-
-#canonico = 'Med_pos_1_pos_3_prueba2.txt'#Cilindro de teflon
-#datos = np.loadtxt(canonico,skiprows=1)
-#f_medida = datos[-51,0]
-#S_medida_sct = datos[-51,1]
-#EzTr_sct_medido = csct_F*abs(10**(S_medida_sct/20))
-#print('Ez_medido: ',EzTr_sct_medido)
-#print('Ez_teorico: ',EzTr_sct)
+##print('start time: ', start_time)
+##print('end time:   ', tm.strftime('%H:%M:%S'))
 
 
-## Si calculamos el coeficiente de calibración con la simulación ($S_{FEM}^{sct}$). Estos coeficientes fueron calculados por simulación con elementos finitos.
-## 
-## $$c_{F_{ls}}^{FEM} = \dfrac{E_{esperado}^{sct}}{S_{FEM}^{sct}}$$
-## 
+### Ahora simulamos el cilindro de Nylon descentrado con lo cual tendremos un valor teórico y, luego, vemos cuál es el valor medido y calibrado con el coeficiente $c_{F_{ls}}^{sct}$.
 
-## In[10]:
+### In[7]:
 
 
-#S_fem = -47.4 #dB con teflón
-#csct_F_FEM = abs(EzTr)/abs(10**(S_fem/20))
-#S_sct_fem = -47.15
-#EzTr_sct_FEM = csct_F_FEM*abs(10**(S_sct_fem/20))
-#print('Ez_calibrado_fem: ',EzTr_sct_FEM)
-#print('Ez_teorico: ',EzTr_sct)
+##r = 35.5e-3/2.0
+##Xc = -0.025
+##Yc = -0.025
+
+##print('Xc:', Xc,'Yc:', Yc,'r:',r)
+
+###Dibujo la geometría generada
+##cilindro = plt.Circle((Xc,Yc),r,fill = False)
+
+###Cargo los parámetros del cilindro
+##cilindro1 = SCATTERER_parameters()
+##cilindro1.epsr = 4.1 #permitividad relativa. Entre [10.0, 80.0]
+##cilindro1.sigma = 0.0
+##cilindro1.f = TRANSMISOR_parameters.f #frecuencia 1.0 GHz (por defecto).
+##cilindro1.radio = r
+##cilindro1.xc = Xc
+##cilindro1.yc = Yc
+
+##print('Permitividad medio:',ACOPLANTE_parameters.epsr)
+##print('Conductividad medio:',ACOPLANTE_parameters.sigma)
+
+##print('Permitividad del cilindro:',cilindro1.epsr)
+##print('Conductividad del cilindro:',cilindro1.sigma)
+
+##figure, axes = plt.subplots()
+##plt.xlim(-0.25/2, 0.25/2)
+##plt.ylim(-0.25/2 , 0.25/2)
+##axes.set_aspect(1)
+##axes.add_artist(cilindro)
+##axes.plot(xantenas,yantenas,'ok')
+##plt.show()
 
 
-## ## Conclusión
-## 
-## 
+### In[8]:
 
-## In[ ]:
+
+##Ezfdtd_sct,eps_data = RunMeep(cilindro1,ACOPLANTE_parameters,TRANSMISOR_parameters, tx, box,RES = 5,calibration = False)
+##EzTr_sct = abs(Ezfdtd_sct)[xRint,yRint]
+
+
+### Ahora comparamos con la medición:
+
+### In[9]:
+
+
+##canonico = 'Med_pos_1_pos_3_prueba2.txt'#Cilindro de teflon
+##datos = np.loadtxt(canonico,skiprows=1)
+##f_medida = datos[-51,0]
+##S_medida_sct = datos[-51,1]
+##EzTr_sct_medido = csct_F*abs(10**(S_medida_sct/20))
+##print('Ez_medido: ',EzTr_sct_medido)
+##print('Ez_teorico: ',EzTr_sct)
+
+
+### Si calculamos el coeficiente de calibración con la simulación ($S_{FEM}^{sct}$). Estos coeficientes fueron calculados por simulación con elementos finitos.
+### 
+### $$c_{F_{ls}}^{FEM} = \dfrac{E_{esperado}^{sct}}{S_{FEM}^{sct}}$$
+### 
+
+### In[10]:
+
+
+##S_fem = -47.4 #dB con teflón
+##csct_F_FEM = abs(EzTr)/abs(10**(S_fem/20))
+##S_sct_fem = -47.15
+##EzTr_sct_FEM = csct_F_FEM*abs(10**(S_sct_fem/20))
+##print('Ez_calibrado_fem: ',EzTr_sct_FEM)
+##print('Ez_teorico: ',EzTr_sct)
+
+
+### ## Conclusión
+### 
+### 
+
+### In[ ]:
 
 
 
